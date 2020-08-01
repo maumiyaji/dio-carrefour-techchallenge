1fba81e0f7f19ea86ae0036db47642343b1fe59b
maumiyaji <maumiyaji@gmail.com>
2020-07-31 23:19:00 -0300

    Finalizando o tech challenge!

--- telegramkeyex.py
+++ telegramkeyex.py
@@ -1,192 +1,225 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""Simple inline keyboard bot with multiple CallbackQueryHandlers.
-This Bot uses the Updater class to handle the bot.
-First, a few callback functions are defined as callback query handler. Then, those functions are
-passed to the Dispatcher and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-Usage:
-Example of a bot that uses inline keyboard that has multiple CallbackQueryHandlers arranged in a
-ConversationHandler.
-Send /start to initiate the conversation.
-Press Ctrl-C on the command line to stop the bot.
 """
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ConversationHandler
-import logging
+Um bot simples, com botões (keyboard) inline para direcionar o cliente
+para uma das áreas principais de atendimento do banco.
 
-# Enable logging
+A base do código foi baseada no exemplo 'inlinekeyboard2.py' em:
+https://github.com/python-telegram-bot/python-telegram-bot/
+
+Lá estão vários exemplos de telegram-bots, cada um com uma característica.
+
+Como o Flask foi utilizado, para rodar o programa, é só seguir a ordem abaixo:
+
+$ export FLASK_APP=telegramkeyex.py
+$ export FLASK_ENV=development
+$ flask run
+"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def coverpage():
+    return render_template("carrefour_telegram.html")
+
+
+import logging
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler,\
+    ConversationHandler, MessageHandler, Filters
+
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
-
 logger = logging.getLogger(__name__)
 
 # Stages
-FIRST, SECOND = range(2)
-# Callback data
-ONE, TWO, THREE, FOUR = range(4)
+FIRST, SECOND, THIRD, FOURTH = range(4)
+
+# Tipos de botões, indexados
+DUV, SUG, CRIT = range(3)
+CARD, SEG, CRED, PAG, PROM, OUTRO = range(6)
+SIM, NAO = range(2)
 
 
+# Função para o /start
 def start(update, context):
-    """Send message on `/start`."""
-    # Get user that sent /start and log his name
+    # Recebe o nome do usuário
     user = update.message.from_user
-    logger.info("User %s started the conversation.", user.first_name)
-    # Build InlineKeyboard where each button has a displayed text
-    # and a string as callback_data
-    # The keyboard is a list of button rows, where each row is in turn
-    # a list (hence `[[...]]`).
+    # InlineKeyboard com os botões iniciais
     keyboard = [
-        [InlineKeyboardButton("1", callback_data=str(ONE)),
-         InlineKeyboardButton("2", callback_data=str(TWO))]
+        [InlineKeyboardButton("Dúvidas", callback_data=str(DUV)),
+         InlineKeyboardButton("Sugestões", callback_data=str(SUG)),
+         InlineKeyboardButton("Críticas", callback_data=str(CRIT))]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    # Send message with text and appended InlineKeyboard
     update.message.reply_text(
-        "Start handler, Choose a route",
-        reply_markup=reply_markup
+        "Olá, {}! Como vai? Sobre qual assunto você gostaria de conversar?".format(user.first_name),
+        reply_markup=reply_markup,
     )
-    # Tell ConversationHandler that we're in state `FIRST` now
+    # Diz ao ConversationHandler que estamos em FIRST
     return FIRST
 
 
+# Função para reiniciar o processo
 def start_over(update, context):
-    """Prompt same text & keyboard as `start` does but not as new message"""
-    # Get CallbackQuery from Update
+    # CallbackQueries, as respostas ao envio dos botões, precisam ser respondidas,
+    # mesmo sem nenhuma mensagem.
+    # https://core.telegram.org/bots/api#callbackquery
     query = update.callback_query
-    # CallbackQueries need to be answered, even if no notification to the user is needed
-    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
     query.answer()
+
     keyboard = [
-        [InlineKeyboardButton("1", callback_data=str(ONE)),
-         InlineKeyboardButton("2", callback_data=str(TWO))]
+        [InlineKeyboardButton("Dúvidas", callback_data=str(DUV)),
+         InlineKeyboardButton("Sugestões", callback_data=str(SUG)),
+         InlineKeyboardButton("Críticas", callback_data=str(CRIT))]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    # Instead of sending a new message, edit the message that
-    # originated the CallbackQuery. This gives the feeling of an
-    # interactive menu.
     query.edit_message_text(
-        text="Start handler, Choose a route",
+        text="Escolha novamente uma das opções:",
         reply_markup=reply_markup
     )
     return FIRST
 
 
-def one(update, context):
-    """Show new choice of buttons"""
+# Função para as Dúvidas
+def duv(update, context):
     query = update.callback_query
     query.answer()
+    # Agora teremos as novas opções de botões
     keyboard = [
-        [InlineKeyboardButton("3", callback_data=str(THREE)),
-         InlineKeyboardButton("4", callback_data=str(FOUR))]
+        [InlineKeyboardButton("Cartão Carrefour", callback_data=str(CARD)),
+         InlineKeyboardButton("Seguros", callback_data=str(SEG))],
+         [InlineKeyboardButton("Crédito Pessoal", callback_data=str(CRED)),
+         InlineKeyboardButton("Pagamento de Contas", callback_data=str(PAG))],
+         [InlineKeyboardButton("Promoções", callback_data=str(PROM)),
+         InlineKeyboardButton("Outros", callback_data=str(OUTRO))]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     query.edit_message_text(
-        text="First CallbackQueryHandler, Choose a route",
+        text="Suas dúvidas são sobre qual dos nossos serviços?",
         reply_markup=reply_markup
     )
-    return FIRST
-
-
-def two(update, context):
-    """Show new choice of buttons"""
-    query = update.callback_query
-    query.answer()
-    keyboard = [
-        [InlineKeyboardButton("1", callback_data=str(ONE)),
-         InlineKeyboardButton("3", callback_data=str(THREE))]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    query.edit_message_text(
-        text="Second CallbackQueryHandler, Choose a route",
-        reply_markup=reply_markup
-    )
-    return FIRST
-
-
-def three(update, context):
-    """Show new choice of buttons"""
-    query = update.callback_query
-    query.answer()
-    keyboard = [
-        [InlineKeyboardButton("Yes, let's do it again!", callback_data=str(ONE)),
-         InlineKeyboardButton("Nah, I've had enough ...", callback_data=str(TWO))]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    query.edit_message_text(
-        text="Third CallbackQueryHandler. Do want to start over?",
-        reply_markup=reply_markup
-    )
-    # Transfer to conversation state `SECOND`
+    # Mudar para outro estado (SECOND)
     return SECOND
 
 
-def four(update, context):
-    """Show new choice of buttons"""
+# Função para as Sugestões
+def sug(update, context):
     query = update.callback_query
     query.answer()
+
     keyboard = [
-        [InlineKeyboardButton("2", callback_data=str(TWO)),
-         InlineKeyboardButton("4", callback_data=str(FOUR))]
+        [InlineKeyboardButton("Cartão Carrefour", callback_data=str(CARD)),
+         InlineKeyboardButton("Seguros", callback_data=str(SEG))],
+         [InlineKeyboardButton("Crédito Pessoal", callback_data=str(CRED)),
+         InlineKeyboardButton("Pagamento de Contas", callback_data=str(PAG))],
+         [InlineKeyboardButton("Promoções", callback_data=str(PROM)),
+         InlineKeyboardButton("Outros", callback_data=str(OUTRO))]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     query.edit_message_text(
-        text="Fourth CallbackQueryHandler, Choose a route",
+        text="Por favor, escolha para qual área você quer enviar sua sugestão:",
         reply_markup=reply_markup
     )
-    return FIRST
+    return SECOND
 
 
-def end(update, context):
-    """Returns `ConversationHandler.END`, which tells the
-    ConversationHandler that the conversation is over"""
+# Função para as Críticas ou Reclamações
+def crit(update, context):
+    query = update.callback_query
+    query.answer()
+    keyboard = [
+        [InlineKeyboardButton("Cartão Carrefour", callback_data=str(CARD)),
+         InlineKeyboardButton("Seguros", callback_data=str(SEG))],
+         [InlineKeyboardButton("Crédito Pessoal", callback_data=str(CRED)),
+         InlineKeyboardButton("Pagamento de Contas", callback_data=str(PAG))],
+         [InlineKeyboardButton("Promoções", callback_data=str(PROM)),
+         InlineKeyboardButton("Outros", callback_data=str(OUTRO))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query.edit_message_text(
+        text="Por favor, escolha para qual área você quer enviar sua crítica ou reclamação:",
+        reply_markup=reply_markup
+    )
+    return SECOND
+
+
+# Pedindo o envio da mensagem do usuário
+def answer(update, context):
     query = update.callback_query
     query.answer()
     query.edit_message_text(
-        text="See you next time!"
+        text="Pode enviar a sua mensagem!"
+    )
+    return THIRD
+
+
+# Resposta genérica para qualquer opção
+def again(update, context):
+    keyboard = [
+        [InlineKeyboardButton("Sim, gostaria.", callback_data=str(SIM)),
+         InlineKeyboardButton("Não, obrigado!", callback_data=str(NAO))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text(
+        "A sua mensagem será enviada para a área responsável e você receberá "
+        "uma resposta em algumas horas!"
+        "\nGostaria de enviar uma mensagem sobre outro assunto?",
+        reply_markup=reply_markup,
+    )
+    return FOURTH
+
+
+# Encerramento da conversa
+def end(update, context):
+    query = update.callback_query
+    query.answer()
+    query.edit_message_text(
+        text="Ok, muito obrigado por conversar conosco!"
+             "\nO Banco Carrefour está sempre buscando melhorar a comunicação com seus clientes!"
     )
     return ConversationHandler.END
 
 
+# Aplicação principal
 def main():
-    # Create the Updater and pass it your bot's token.
-    updater = Updater("1360249663:AAG0Fcwt1_CIApjuIP-wtmajgSHrjTozgvM", use_context=True)
+    # Cria o Updater e envia o token do Bot
+    updater = Updater("1395822914:AAGk4yAKqPSBvD14-ZLOlgzu5zafdDr4m3I", use_context=True)
 
-    # Get the dispatcher to register handlers
+    # através do 'dispatcher' iremos cadastrar os nossos comandos no Bot
     dp = updater.dispatcher
 
-    # Setup conversation handler with the states FIRST and SECOND
-    # Use the pattern parameter to pass CallbackQueries with specific
-    # data pattern to the corresponding handlers.
-    # ^ means "start of line/string"
-    # $ means "end of line/string"
-    # So ^ABC$ will only allow 'ABC'
+    # Setup do ConversationHandler com os 'estados' FIRST, QUALDUV, QUALSUG, QUALCRIT ou DENOVO
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states={
-            FIRST: [CallbackQueryHandler(one, pattern='^' + str(ONE) + '$'),
-                    CallbackQueryHandler(two, pattern='^' + str(TWO) + '$'),
-                    CallbackQueryHandler(three, pattern='^' + str(THREE) + '$'),
-                    CallbackQueryHandler(four, pattern='^' + str(FOUR) + '$')],
-            SECOND: [CallbackQueryHandler(start_over, pattern='^' + str(ONE) + '$'),
-                     CallbackQueryHandler(end, pattern='^' + str(TWO) + '$')]
+            FIRST: [CallbackQueryHandler(duv, pattern='^' + str(DUV) + '$'),
+                    CallbackQueryHandler(sug, pattern='^' + str(SUG) + '$'),
+                    CallbackQueryHandler(crit, pattern='^' + str(CRIT) + '$')],
+            SECOND: [CallbackQueryHandler(answer, pattern='^' + str(CARD) + '$'),
+                     CallbackQueryHandler(answer, pattern='^' + str(SEG) + '$'),
+                     CallbackQueryHandler(answer, pattern='^' + str(CRED) + '$'),
+                     CallbackQueryHandler(answer, pattern='^' + str(PAG) + '$'),
+                     CallbackQueryHandler(answer, pattern='^' + str(PROM) + '$'),
+                     CallbackQueryHandler(answer, pattern='^' + str(OUTRO) + '$')],
+            THIRD: [MessageHandler(Filters.text & ~Filters.command, again)],
+            FOURTH: [CallbackQueryHandler(start_over, pattern='^' + str(SIM) + '$'),
+                     CallbackQueryHandler(end, pattern='^' + str(NAO) + '$')],
         },
         fallbacks=[CommandHandler('start', start)]
     )
 
-    # Add ConversationHandler to dispatcher that will be used for handl
-    # ing
-    # updates
+    # Aqui nós adicionamos o ConversationHandler acima para o dispatcher,
+    # para gerenciar as mudanças que cada botão vai provocar
     dp.add_handler(conv_handler)
 
-    # Start the Bot
+    # Aqui nós iniciamos o Bot, pesquisando qualquer atualização enviada para ele
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
+    # O idle() é para 'bloquear' a aplicação até que um comando ou o Ctrl-C pare o updater
+    # comandos: SIGINIT, SIGTERM, SIGABRT
     updater.idle()
 
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
